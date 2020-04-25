e8f8541d1b43febad081a69868663e2a07f6c3a4
trantienthuong <thuongtt@7saturday.com>
2020-04-25 11:19:35 +0700

    axios,asyncData ,vuex

--- pages/videos/index.vue
+++ pages/videos/index.vue
@@ -3,7 +3,7 @@
     <div>
       <h1>Videos</h1>
       <div class="video-container">
-        <div v-for="video in videos" :key="video.name">
+        <div v-for="video in $store.state.videos" :key="video.name">
           <div class="video-box">
             <img :src="video.thumbnail" />
             <div>
@@ -19,43 +19,34 @@
 </template>
 
 <script>
-import Logo from "~/components/Logo.vue";
-import VuetifyLogo from "~/components/VuetifyLogo.vue";
-
+import axios from "axios";
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  },
-  data() {
-    return {
-      videos: [
-        {
-          id: 1,
-          name: "ES2015 Arrow Functions",
-          description: `<p>ES2015 (aka ES6) has some great ways to make your code easier to write and understand. In this episode, we cover two different ways that you can make your code clearer by removing the 'function' keyword.</p>`,
-          thumbnail:
-            "https://vue-screencasts.s3.us-east-2.amazonaws.com/images/video-thumbnails/Thumbnail+-+Arrow+Function.png"
-        },
-        {
-          id: 2,
-          name: "ES2015 Template Strings",
-          description: `<p>Template strings are an incredibly useful new feature in ES2015... and you can use them in your Ember apps today!</p>
-                        <p>Here are 3 cool things that template strings enable.</p>`,
-          thumbnail:
-            "https://vue-screencasts.s3.us-east-2.amazonaws.com/images/video-thumbnails/Thumbnail+-+Template+Strings.png"
-        },
-        {
-          id: 5,
-          name: "ES2015 Modules",
-          description: `<p>Before modules, javascript code loading was a mess. Now it's pretty amazing.</p>
-                        <p>Learn about ES2015 modules and how they work together with ember-cli.</p>`,
-          thumbnail:
-            "https://vue-screencasts.s3.us-east-2.amazonaws.com/images/video-thumbnails/Thumbnail+-+ES2015+Modules.png"
-        }
-      ]
-    };
+  // async asyncData({$axios}) {
+  //   let response = await $axios.get('/videos')
+  //   //data có thể lấy từ server hoặc fake 1 file Json
+  //   let videos = response.data.data.map(v => v.attributes);
+  //   return {
+  //     videos
+  //   }
+  // },
+  async fetch({ $axios, store }) {
+    let response = await $axios.get("/videos");
+    let videos = response.data.data.map(v => v.attributes);
+    store.commit("SET_VIDEOS", videos);
   }
+  // data() {
+  //   return {
+  //     videos: []
+  //   };
+  // },
+  // async created() {
+  //   try {
+  //     const response = await axios.get(`http://localhost:3000/videos`);
+  //     this.videos = response.data;
+  //   }catch(e){
+  //     console.log('tag', 'e')
+  //   }
+  // }
 };
 </script>
 <style scoped lang="scss">
